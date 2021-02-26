9c4f9fb649da568b9fdc903847bcd46075ed0304
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-02-26 15:04:24 +0100

    api - not done (Blocked by CORS)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,79 @@
+import { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Layout from './layout';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
-  );
+interface Props {}
+interface State {
+  images: CocktailPhoto[];
 }
+interface CocktailResponse {
+  drinks: [];
+
+}
+class App extends Component<Props, State> {
+  private readonly API_KEY = '1';
+  private readonly COCKTAILS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f='; // Kanske punkt på slutet
+  
+  
+  state: State = {
+    images: []
+  }
+
+  handleNewSearchValue = (value: string) => {
+    this.fetchImagesFromApi(value);
+  };
+
+  
+  async fetchImagesFromApi(searchValue: string) {
+    try {
+      const url = this.COCKTAILS_URL + searchValue;
+
+      const response = await fetch(url, {
+        headers: { 'Authorization': this.API_KEY }
+      });
+      
+      const result: CocktailResponse = await response.json();
+      this.setState({ images: result.drinks || [] })
+
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Layout 
+          onChange={this.handleNewSearchValue}
+        />
+      </BrowserRouter>
+    );
+  }
+}
+
 export default App;
+
+export interface CocktailPhoto {
+  // drinks: [{drinkObject}]
+  strDrink: string;
+  //strGlass: string;
+  //strInstructions: string;
+  //strDrinkThumb: string;
+  //strIngredient: []
+  /*  "strIngredient1": "Galliano",
+      "strIngredient2": "Ginger ale",
+      "strIngredient3": "Ice",
+      "strIngredient4": null,
+      "strIngredient5": null,
+      "strIngredient6": null,
+      "strIngredient7": null,
+      "strIngredient8": null,
+  */
+ //strMeasure: []
+ /*   "strMeasure1": "2 1/2 shots ",
+      "strMeasure2": null,
+      "strMeasure3": null,
+      "strMeasure4": null,
+      "strMeasure5": null,
+*/
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,43 @@
 import Header from './header';
 import Main from './main';
 import Footer from './footer';
+import { ChangeEvent, Component } from 'react';
 
-function Layout() {
-   return (
-      <div>
-         <Header />
-         <Main />
-         <Footer />
-      </div>
-   );
+interface Props {
+   onChange: (value: string) => void;
+}
+interface State {
+   value: string;
+}
+class Layout extends Component<Props, State> {
+   state: State = { value: '' }
+   
+
+   handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+      this.setState({ value: event.target.value })
+   }
+
+   componentDidUpdate(_: Props) {
+      const { onChange } = this.props;
+      onChange(this.state.value)
+   }
+   
+
+   render() {
+      return (
+         <div>
+            <input 
+               placeholder="Search for cocktails"
+               value={this.state.value}
+               onChange={this.handleChange}
+            />
+   
+            <Header />
+            <Main />
+            <Footer />
+         </div>
+      );
+   }
 }
 
 export default Layout;
