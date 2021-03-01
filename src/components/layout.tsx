9f93fa048a8f55003b9ba04883c3948f9f42d40f
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-01 13:45:33 +0100

    deleted unnecessary component and link

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,11 @@
 import Header from './header';
 import Main from './main';
-import Footer from './footer';
 
 function Layout() {
    return (
       <div>
          <Header />
          <Main />
-         <Footer />
       </div>
    );
 }
