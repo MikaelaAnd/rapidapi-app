e343751fd081bd28c2eea5caa832c0d20e623ac6
Mikaela Andersson <mikaelaaand@gmail.com>
2021-03-02 11:07:37 +0100

    addded ingrediens, instructions and images

--- src/components/searchResult.tsx
+++ src/components/searchResult.tsx
@@ -1,14 +1,22 @@
+import './searchResult.css';
 
 function SearchResult() {
     return (
-    <div>
-        <p>Här kommer en del bilder</p>
-        <p>Här kommer en del bilder</p>
-        <p>Här kommer en del bilder</p>
-        <p>Här kommer en del bilder</p>
+    <div className="wrap-style">
+        <div className="drink-photo">  </div>
+        <h3 className="search-element"> </h3>
+        <div className="ingredient-list">
+            <span className="ingredients"> </span>
+            <span className="ingredients"> </span>
+            <span className="ingredients"> </span>
+            <span className="ingredients"> </span>
+            <span className="ingredients"> </span>
+            <span className="ingredients"> </span>
+            <span className="ingredients"> </span>
+        </div>
+        <p className="instructions"> </p>
     </div>
     );
 }
-
 
 export default SearchResult;
--- src/components/startpage.tsx
+++ src/components/startpage.tsx
@@ -27,25 +27,65 @@ function Startpage() {
     return (
         <div>
             <Link to="/searchResult">
-                <input placeholder="search"></input>
+                <button onClick={getRandomCocktail}>
+                    Get random drink
+                </button>
             </Link>
-            <p className="search-element"></p>
         </div>
     );
 } 
-getRandomCocktail();
 
 function displayRandomCocktail(cocktail: any) {
     console.log(cocktail.drinks[0].strDrink);
 
-let drinkSection = document.querySelector('.search-element');
+let drink = document.querySelector('.search-element');
+let drinkInstructions = document.querySelector('.instructions');
+let drinkIngredients = document.querySelector('.ingredients');
+let drinkPhoto = document.querySelector('.drink-photo');
 
-let drinkName = document.createElement('p');
+let drinkName = document.createElement('h3');
+let img = document.createElement('img');
+let instructions = document.createElement('p');
+let ingredients1 = document.createElement('span');
+let ingredients2 = document.createElement('span');
+let ingredients3 = document.createElement('span');
+let ingredients4 = document.createElement('span');
+let ingredients5 = document.createElement('span');
+let ingredients6 = document.createElement('span');
+let ingredients7 = document.createElement('span');
+let ingredients8 = document.createElement('span');
+let ingredients9 = document.createElement('span');
+let ingredients10 = document.createElement('span');
+
 drinkName.innerHTML = cocktail.drinks[0].strDrink;
+img.src = cocktail.drinks[0].strDrinkThumb;
+instructions.innerHTML = cocktail.drinks[0].strInstructions;
+ingredients1.innerHTML = cocktail.drinks[0].strIngredient1;
+ingredients2.innerHTML = cocktail.drinks[0].strIngredient2;
+ingredients3.innerHTML = cocktail.drinks[0].strIngredient3;
+ingredients4.innerHTML = cocktail.drinks[0].strIngredient4;
+ingredients5.innerHTML = cocktail.drinks[0].strIngredient5;
+ingredients6.innerHTML = cocktail.drinks[0].strIngredient6;
+ingredients7.innerHTML = cocktail.drinks[0].strIngredient7;
+ingredients8.innerHTML = cocktail.drinks[0].strIngredient8;
+ingredients9.innerHTML = cocktail.drinks[0].strIngredient9;
+ingredients10.innerHTML = cocktail.drinks[0].strIngredient10;
 
-drinkSection?.appendChild(drinkName);
+
+drinkInstructions?.appendChild(instructions);
+drinkPhoto?.appendChild(img);
+drink?.appendChild(drinkName);
+drinkIngredients?.appendChild(ingredients1);
+drinkIngredients?.appendChild(ingredients2);
+drinkIngredients?.appendChild(ingredients3);
+drinkIngredients?.appendChild(ingredients4);
+drinkIngredients?.appendChild(ingredients5);
+drinkIngredients?.appendChild(ingredients6);
+drinkIngredients?.appendChild(ingredients7);
+drinkIngredients?.appendChild(ingredients8);
+drinkIngredients?.appendChild(ingredients9);
+drinkIngredients?.appendChild(ingredients10);
 }
-
 
 
 export default Startpage;
